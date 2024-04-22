292a9be2f1ba90955c01f5aa0ebbaaa8f3a44c9e
Leehyoungwoo <dlguddn3024@gmail.com>
2024-04-22 14:51:47 +0900

    :sparkles: feat file entity and file type

---
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/file/entity/File.java
@@ -0,0 +1,37 @@
+package io.ssafy.gatee.domain.file.entity;
+
+import io.ssafy.gatee.domain.file.entity.type.FileType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Column(name = "original_name")
+    private String originalName;
+
+    @NotNull
+    private String url;
+
+    @NotNull
+    private String dir;
+
+    @Enumerated(EnumType.STRING)
+    private FileType fileType;
+}
---
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/file/entity/type/FileType.java
@@ -0,0 +1,8 @@
+package io.ssafy.gatee.domain.file.entity.type;
+
+public enum FileType {
+    MEMBER_PROFILE,
+    EMOTION,
+    ALBUM,
+    MESSAGE
+}
