d6e8b011b652f919a9b6bc5f26095c26b407345b
Leehyoungwoo <dlguddn3024@gmail.com>
2024-04-22 13:26:13 +0900

    :tada: first init

---
+++ back/gatee/src/main/java/io/ssafy/gatee/GateeApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.gatee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateeApplication.class, args);
+	}
+
+}
---
+++ back/gatee/src/test/java/io/ssafy/gatee/GateeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssafy.gatee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
