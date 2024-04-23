809b2fe415c47eb109055ba6d61e26524f0e2beb
이윤정 <2yunj007@gmail.com>
2024-04-23 17:51:32 +0900

    [FE #1] set Sass & page layout

--- front/gatee/src/Router.js
+++ front/gatee/src/Router.js
@@ -39,42 +39,44 @@ const Router = () => {
   return (
       <BrowserRouter>
         <TopBar/>
-        <Routes>
-            {/*온보딩*/}
-            <Route path="/" element={<OnboardingIndex />} />
-            {/*회원가입*/}
-            <Route path="/signup" element={<SignupIndex />} />
-            <Route path="/signup/member" element={<SignupMember />} />
-            <Route path="/signup/family" element={<SignupFamily />} />
-            {/*모의고사*/}
-            <Route path="/exam" element={<ExamIndex />} />
-            <Route path="/exam/grade" element={<ExamGrade />} />
-            <Route path="/exam/scored/:id" element={<ExamScored />} />
-            <Route path="/exam/taking" element={<ExamTaking />} />
-            {/*채팅*/}
-            <Route path="/chat" element={<ChatIndex />} />
-            {/*알람*/}
-            <Route path="/notification" element={<NotificationIndex />} />
-            {/*프로필*/}
-            <Route path="/profile" element={<ProfileIndex />} />
-            {/*메인*/}
-            <Route path="/main" element={<MainIndex />} />
-            <Route path="/main/mission" element={<MissionIndex />} />
-            {/*스케줄*/}
-            <Route path="/schedule" element={<ScheduleIndex />} />
-            <Route path="/schedule/create-schedule" element={<ScheduleCreateSchedule />} />
-            <Route path="/schedule/create-review" element={<ScheduleCreateReview />} />
-            <Route path="/schedule/:id" element={<ScheduleDetail />} />
-            {/*백과사전*/}
-            <Route path="/character" element={<CharacterIndex />} />
-            <Route path="/character/start" element={<CharacterStart />} />
-            <Route path="/character/question" element={<CharacterQuestion />} />
-            {/*앨범*/}
-            <Route path="/photo" element={<PhotoAlbumIndex />} />
-            <Route path="/photo/album" element={<PhotoAlbumPhoto />} />
-            <Route path="/photo/group/:id" element={<PhotoAllGroupDetail />} />
-            <Route path="/photo/album/:id" element={<PhotoAlbumGroupDetail />} />
-        </Routes>
+          <div className="container">
+              <Routes>
+                  {/*온보딩*/}
+                  <Route path="/" element={<OnboardingIndex />} />
+                  {/*회원가입*/}
+                  <Route path="/signup" element={<SignupIndex />} />
+                  <Route path="/signup/member" element={<SignupMember />} />
+                  <Route path="/signup/family" element={<SignupFamily />} />
+                  {/*모의고사*/}
+                  <Route path="/exam" element={<ExamIndex />} />
+                  <Route path="/exam/grade" element={<ExamGrade />} />
+                  <Route path="/exam/scored/:id" element={<ExamScored />} />
+                  <Route path="/exam/taking" element={<ExamTaking />} />
+                  {/*채팅*/}
+                  <Route path="/chat" element={<ChatIndex />} />
+                  {/*알람*/}
+                  <Route path="/notification" element={<NotificationIndex />} />
+                  {/*프로필*/}
+                  <Route path="/profile" element={<ProfileIndex />} />
+                  {/*메인*/}
+                  <Route path="/main" element={<MainIndex />} />
+                  <Route path="/main/mission" element={<MissionIndex />} />
+                  {/*스케줄*/}
+                  <Route path="/schedule" element={<ScheduleIndex />} />
+                  <Route path="/schedule/create-schedule" element={<ScheduleCreateSchedule />} />
+                  <Route path="/schedule/create-review" element={<ScheduleCreateReview />} />
+                  <Route path="/schedule/:id" element={<ScheduleDetail />} />
+                  {/*백과사전*/}
+                  <Route path="/character" element={<CharacterIndex />} />
+                  <Route path="/character/start" element={<CharacterStart />} />
+                  <Route path="/character/question" element={<CharacterQuestion />} />
+                  {/*앨범*/}
+                  <Route path="/photo" element={<PhotoAlbumIndex />} />
+                  <Route path="/photo/album" element={<PhotoAlbumPhoto />} />
+                  <Route path="/photo/group/:id" element={<PhotoAllGroupDetail />} />
+                  <Route path="/photo/album/:id" element={<PhotoAlbumGroupDetail />} />
+              </Routes>
+          </div>
         <BottomBar style={{position: "fixed",bottom: 0, left: 0, right: 0}}/>
       </BrowserRouter>
   );
