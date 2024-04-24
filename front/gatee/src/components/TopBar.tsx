4ac9d7ab33ab7650fa0285d54142e604d8313680
asthyeon <asthyeon@naver.com>
2024-04-24 09:20:10 +0900

    :memo: [FE] update TopBar

--- front/gatee/src/components/TopBar.tsx
+++ front/gatee/src/components/TopBar.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { IoChevronBack } from "react-icons/io5";
+import { IoPersonOutline } from "react-icons/io5";
+import { GoBell } from "react-icons/go";
 
-function TopBar (){
+function TopBar () {
     const navigate = useNavigate();
-    // 가려는 목적지를 문자로 받으면, 이동시켜주는 함수
-    const goto = function(destination:String){
-        navigate(`/${destination}`);}
-    // 뒤로는 연결 못함
-    const back = function(): void {
+
+    // 가려는 목적지를 문자열로 받으면, 이동시켜주는 함수
+    const goTo = (destination: string) => {
+        navigate(`/${destination}`);
+    }
+
+    // 바로 이전 페이지로 이동
+    const goBack = () => {
         navigate(-1);
     }
-    return <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-        <button onClick={()=>back()}>
-            &lt;
-        </button>
-        <div style={{display: 'flex', flexDirection: 'row', gap:10}}>
-        <button onClick={()=>goto("notification")}>알림</button>
-        <button onClick={()=>goto("profile")}>프로필</button>
+
+    return <div className="topBar">
+        <div className="topBar__leftDiv" onClick={goBack}>
+            <IoChevronBack/>
+        </div>
+        <div className="topBar__rightDiv">
+        <div onClick={() => goTo("notification")}>
+            <GoBell />
+        </div>
+        <div onClick={() => goTo("profile")}>
+            <IoPersonOutline />
+        </div>
         </div>
     </div>
 }
