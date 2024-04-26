efe77612cb2e2e5784bdf386ebef2153116afcf7
Leehyoungwoo <dlguddn3024@gmail.com>
2024-04-26 11:04:20 +0900

    :memo:[BE] write blue-green deploy ci file

--- back/gatee/devops/nginx/detect_env.sh
+++ back/gatee/devops/nginx/detect_env.sh
@@ -6,9 +6,9 @@
 NGINX_CONFIG_FILE="/etc/nginx/nginx.conf"
 
 # NGINX 설정 파일에서 'upstream springboot_app' 섹션을 검색하여, 현재 활성화된 서버가 블루인지 그린인지 파악
-if grep -q "gatee-api-blue:8080" $NGINX_CONFIG_FILE; then
+if grep -q "gatee-api-blue" $NGINX_CONFIG_FILE; then
   echo "blue"
-elif grep -q "gatee-api-green:8081" $NGINX_CONFIG_FILE; then
+elif grep -q "gatee-api-green" $NGINX_CONFIG_FILE; then
   echo "green"
 else
   echo "blue"
