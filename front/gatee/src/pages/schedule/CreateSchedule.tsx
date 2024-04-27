06007a28242d8c746608e8aa10aa4942f80b107d
2yunj007 <2yunj007@gmail.com>
2024-04-27 22:34:49 +0900

    :sparkles: [FE] add temporary data

--- front/gatee/src/pages/schedule/CreateSchedule.tsx
+++ front/gatee/src/pages/schedule/CreateSchedule.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FamilyMemberInfoSample } from "src/constants";
 
 function ScheduleCreateSchedule() {
-    return (
-        <div>ScheduleCreateSchedule</div>
-    );
+  return (
+    <div className="create-schedule-container">
+      <div className="create-schedule-title">새로운 일정 등록</div>
+
+    {/*일정 설명*/}
+    </div>
+  );
 }
 
 export default ScheduleCreateSchedule;
