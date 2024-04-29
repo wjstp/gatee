cbd413ea581f8ac686b15473e837bc56e5b2b118
megar0829 <megar0829@gmail.com>
2024-04-29 17:16:15 +0900

    :sparkles: [BE] Update - Test code annotation

--- back/gatee/src/test/java/io/ssafy/gatee/domain/album/api/AlbumControllerTest.java
+++ back/gatee/src/test/java/io/ssafy/gatee/domain/album/api/AlbumControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles({"common, prod"})
 @AutoConfigureRestDocs
 @WebMvcTest(AlbumController.class)
 @MockBean(JpaMetamodelMappingContext.class)
