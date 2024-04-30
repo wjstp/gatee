e90bf9a74971f758fb11069ffe44a74cd3fee051
dlguddn3024 <dlguddn3024@gmail.com>
2024-05-01 02:11:31 +0900

    :recycle: remove actuator url for deny in nginx

--- back/gatee/src/main/java/io/ssafy/gatee/global/security/config/SecurityConfig.java
+++ back/gatee/src/main/java/io/ssafy/gatee/global/security/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
 
     private final String[] URL_WHITE_LIST = {
             "/api/jwt/**", "/api/auth/**", "/error",
-            "/docs/**"
+            "/docs/**", "/actuator/**"
     };
 
     private final JwtService jwtService;
