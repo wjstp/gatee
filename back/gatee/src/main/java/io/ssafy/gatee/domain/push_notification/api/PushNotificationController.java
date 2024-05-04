cd9b6034534958a78fc78d8f33edac0f4556929d
wjstp <wjstp14@gmail.com>
2024-05-04 17:03:31 +0900

    :recycle: [BE] modify fcm controller request

--- back/gatee/src/main/java/io/ssafy/gatee/domain/push_notification/api/PushNotificationController.java
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/push_notification/api/PushNotificationController.java
@@ -2,6 +2,7 @@ package io.ssafy.gatee.domain.push_notification.api;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
 import io.ssafy.gatee.domain.push_notification.application.PushNotificationService;
+import io.ssafy.gatee.domain.push_notification.dto.request.TokenReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +16,9 @@ public class PushNotificationController {
     private final PushNotificationService notificationService;
 
     @PostMapping("/test")
-    public String testNotice(@RequestBody String token) throws FirebaseMessagingException {
-        notificationService.sendTestPush(token);
+    public String testNotice(@RequestBody TokenReq tokenReq) throws FirebaseMessagingException {
+        System.out.println(tokenReq.token());
+        notificationService.sendTestPush(tokenReq.token());
         return "notification success";
     }
 }
