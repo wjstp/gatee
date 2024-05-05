72c049e19b58287ce916eb59d262ca6607bd4625
yebin <yebin4684@gmail.com>
2024-05-05 12:43:28 +0900

    :bug:[FE] Local Axios Test

--- front/gatee/src/App.tsx
+++ front/gatee/src/App.tsx
@@ -1,13 +1,12 @@
 import React, {useEffect} from 'react';
 import Router from "./Router";
-import {requestPermission} from "./firebase-messaging-sw";
-import {useMemberStore} from "@store/useMemberStore";
+// import {requestPermission} from "./firebase-messaging-sw";
 import {getPushAlarmByLocalStorage} from "@api/FirebaseAxios";
 
 const App = ()=> {
-  const {accessToken} = useMemberStore()
+
   useEffect(() => {
-    getPushAlarmByLocalStorage(accessToken)
+    getPushAlarmByLocalStorage()
     // requestPermission(accessToken)
   }, []);
   // 로컬 스토리지 초기화
