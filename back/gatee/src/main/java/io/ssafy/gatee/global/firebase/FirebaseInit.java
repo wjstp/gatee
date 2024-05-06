fc5e7f3bc69a013598dd029b441379af826d689c
wjstp <wjstp14@gmail.com>
2024-05-06 19:06:27 +0900

    :bug:[BE]fix setting

--- back/gatee/src/main/java/io/ssafy/gatee/global/firebase/FirebaseInit.java
+++ back/gatee/src/main/java/io/ssafy/gatee/global/firebase/FirebaseInit.java
@@ -13,8 +13,7 @@ import java.io.IOException;
 @Service
 public class FirebaseInit {
 
-    @Value("${spring.data.firebase.key-path}")
-    private String PATH;
+    private String PATH ="gatee-bf33f-firebase-adminsdk-va5su-a0d2f8f65b.json";
     private boolean isInitialized = false;
 
     // 의존성 주입 완료 후 실행, 생성자보다 늦게 호출, 다른 리소스에서 호출되지 않아도 수행된다
