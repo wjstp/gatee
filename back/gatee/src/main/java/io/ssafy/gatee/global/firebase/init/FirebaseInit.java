f180d8e41fa06bf2a5e31143794b1fc67c87b267
wjstp <wjstp14@gmail.com>
2024-05-07 17:10:03 +0900

    :recycle:[BE] modify cors setting

--- back/gatee/src/main/java/io/ssafy/gatee/global/firebase/init/FirebaseInit.java
+++ back/gatee/src/main/java/io/ssafy/gatee/global/firebase/init/FirebaseInit.java
@@ -4,7 +4,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
