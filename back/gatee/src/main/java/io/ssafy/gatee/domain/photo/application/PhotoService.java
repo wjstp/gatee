82144cf2308669efa76a51a062c808b6e8313a33
megar0829 <megar0829@gmail.com>
2024-05-10 15:24:54 +0900

    :sparkles: [BE] Create - Delete Photo List Api Logic

--- back/gatee/src/main/java/io/ssafy/gatee/domain/photo/application/PhotoService.java
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/photo/application/PhotoService.java
@@ -1,6 +1,7 @@
 package io.ssafy.gatee.domain.photo.application;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
+import io.ssafy.gatee.domain.photo.dto.request.PhotoListDeleteReq;
 import io.ssafy.gatee.domain.photo.dto.request.PhotoListReq;
 import io.ssafy.gatee.domain.photo.dto.request.PhotoSaveReq;
 import io.ssafy.gatee.domain.photo.dto.response.PhotoDetailRes;
@@ -19,6 +20,7 @@ public interface PhotoService {
     PhotoDetailRes readPhotoDetail(Long photoId, UUID memberId);
     PhotoSaveRes savePhoto(PhotoSaveReq photoSaveReq, UUID memberId) throws FirebaseMessagingException;
     void deletePhoto(UUID familyId, Long photoId, UUID memberId) throws DoNotHavePermissionException;
+    void deletePhotoList(PhotoListDeleteReq photoListDeleteReq);
     void savePhotoReaction(UUID memberId, Long photoId);
     void deletePhotoReaction(UUID memberId, Long photoId);
 }
