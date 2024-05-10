ef5f0e5476f6516eaa9af7cb2690703f571fa9eb
megar0829 <megar0829@gmail.com>
2024-05-10 09:49:30 +0900

    :sparkles: [BE] Update - Family Api Request

--- back/gatee/src/main/java/io/ssafy/gatee/domain/photo/api/PhotoController.java
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/photo/api/PhotoController.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/photos")
