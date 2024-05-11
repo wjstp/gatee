6b2513536c8b465c8ac64ffc643043d7fc9113ca
wjstp <wjstp14@gmail.com>
2024-05-11 21:20:46 +0900

    :recycle:[BE]modify login response

--- back/gatee/src/main/java/io/ssafy/gatee/global/security/handler/CustomOAuth2SuccessHandler.java
+++ back/gatee/src/main/java/io/ssafy/gatee/global/security/handler/CustomOAuth2SuccessHandler.java
@@ -1,6 +1,7 @@
 package io.ssafy.gatee.global.security.handler;
 
 import io.ssafy.gatee.global.jwt.application.JwtService;
+import io.ssafy.gatee.global.security.user.CustomUserDetails;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,10 +23,9 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("로그인 성공");
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        log.info("test" + customUserDetails.getMemberId());
         jwtService.publishTokens(response, authentication);
         response.setStatus(200);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("{\"message\":\"로그인되었습니다.\"}");
     }
 }
