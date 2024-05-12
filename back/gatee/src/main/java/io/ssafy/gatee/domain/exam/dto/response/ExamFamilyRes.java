9268bf0d8c5e21e6066925a2e63e3d7da8d4aa19
wjstp <wjstp14@gmail.com>
2024-05-12 22:19:28 +0900

    :docs:[BE] add exam family api to restdocs

--- back/gatee/src/main/java/io/ssafy/gatee/domain/exam/dto/response/ExamFamilyRes.java
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/exam/dto/response/ExamFamilyRes.java
@@ -1,7 +1,10 @@
 package io.ssafy.gatee.domain.exam.dto.response;
 
+import lombok.Builder;
+
 import java.util.UUID;
 
+@Builder
 public record ExamFamilyRes(
         String nickname,
         UUID memberId,
