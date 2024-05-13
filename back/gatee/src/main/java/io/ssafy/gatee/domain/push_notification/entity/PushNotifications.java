f222ca3acfc653504877a53bd5ec4e86dbd2539b
wjstp <wjstp14@gmail.com>
2024-05-13 12:43:56 +0900

    :recycle:[BE] modify redirect uri

--- back/gatee/src/main/java/io/ssafy/gatee/domain/push_notification/entity/PushNotifications.java
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/push_notification/entity/PushNotifications.java
@@ -1,15 +1,10 @@
 package io.ssafy.gatee.domain.push_notification.entity;
 
 
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-import java.util.UUID;
 
 @Getter
 @Document(collection = "push_notification")
