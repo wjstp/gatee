bf26664ecc9204efe557f20fbd00eae439e59789
wjstp <wjstp14@gmail.com>
2024-05-13 12:45:39 +0900

    :recycle:[BE]modify redirect uri

--- back/gatee/src/main/java/io/ssafy/gatee/global/security/handler/CustomOAuth2SuccessHandler.java
+++ back/gatee/src/main/java/io/ssafy/gatee/global/security/handler/CustomOAuth2SuccessHandler.java
@@ -33,9 +33,9 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
 
     private final String REDIRECT_URI_SUCCESS = "/main";
 
-    private final String REDIRECT_URI_NOT_FAMILY = "/signup/family-set";
+    private final String REDIRECT_URI_NOT_FAMILY = "/signup";
 
-    private final String REDIRECT_URI_ANONYMOUS = "/signup/family-set";
+    private final String REDIRECT_URI_ANONYMOUS = "/signup/member-set";
 
 
     @Override
