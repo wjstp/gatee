f009828eecaee7f3acc96c92d167684146c46d24
Leehyoungwoo <dlguddn3024@gmail.com>
2024-05-13 12:29:56 +0900

    :sparkles: feat auth service to prevent unAuthorized member to read data

--- back/gatee/src/main/java/io/ssafy/gatee/domain/appointment/api/AppointmentController.java
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/appointment/api/AppointmentController.java
@@ -1,7 +1,31 @@
 package io.ssafy.gatee.domain.appointment.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.ssafy.gatee.domain.appointment.application.AppointmentService;
+import io.ssafy.gatee.domain.appointment.dto.JoinMembersDto;
+import io.ssafy.gatee.global.security.application.AuthorizeService;
+import io.ssafy.gatee.global.security.user.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/appointments")
+@RequiredArgsConstructor
 public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+    private final AuthorizeService authorizeService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{appointmentId}")
+    @PreAuthorize("@authorizeService.authorizeToReadAppointment(#principal.getMemberId, #appointmentId)")
+    public JoinMembersDto getJoinMemberInAppointment(
+            @PathVariable Long appointmentId,
+            @AuthenticationPrincipal CustomUserDetails principal) {
+        return appointmentService.getJoinMemberInAppointment(appointmentId);
+    }
 }
