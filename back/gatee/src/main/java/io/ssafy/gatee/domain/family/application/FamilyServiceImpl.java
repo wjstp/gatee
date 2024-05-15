f157c8a86d8ca981c0e3304e2ac5f6f042d185c7
wjstp <wjstp14@gmail.com>
2024-05-15 13:24:09 +0900

    :bug:family image null exception

--- back/gatee/src/main/java/io/ssafy/gatee/domain/family/application/FamilyServiceImpl.java
+++ back/gatee/src/main/java/io/ssafy/gatee/domain/family/application/FamilyServiceImpl.java
@@ -222,13 +222,15 @@ public class FamilyServiceImpl implements FamilyService {
                     .mood(memberFamily.getMember().getMood())
                     .isLeader(memberFamily.isLeader())
                     .birthType(memberFamily.getMember().getBirthType())
-                    .profileImageUrl(memberFamily.getMember().getFile().getUrl())
+                    .profileImageUrl(Objects.nonNull(memberFamily.getMember().getFile())?
+                            memberFamily.getMember().getFile().getUrl():null)
                     .phoneNumber(memberFamily.getMember().getPhoneNumber())
                     .build()).toList();
             return FamilyInfoRes.builder()
                     .name(family.getName())
                     .familyScore(family.getScore())
-                    .familyImageUrl(family.getFile().getUrl())
+                    .familyImageUrl(Objects.nonNull(family.getFile())?
+                            family.getFile().getUrl():null)
                     .memberFamilyInfoList(memberFamilyInfoList)
                     .build();
         }
