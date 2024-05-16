92b18858f03659217dff71a541faef574c61735b
wjstp <wjstp14@gmail.com>
2024-05-16 12:29:08 +0900

    :sparkles:[BE] feat batch

--- back/gatee/src/main/java/io/ssafy/gatee/global/batch/config/FeatureNotificationBatchConfig.java
+++ back/gatee/src/main/java/io/ssafy/gatee/global/batch/config/FeatureNotificationBatchConfig.java
@@ -38,73 +38,74 @@ import java.util.UUID;
 @Configuration
 @RequiredArgsConstructor
 public class FeatureNotificationBatchConfig {
-//    private final int CHUNK_SIZE = 1000;
-//    private final String JOB_NAME = "featureNotificationJob";
-//    private final EntityManagerFactory entityManagerFactory;
-//    private final MemberNotificationRepository memberNotificationRepository;
-//    private final PushNotificationService pushNotificationService;
-//    private final MemberFeatureRepository memberFeatureRepository;
-//    private final MemberFamilyRepository memberFamilyRepository;
-//
-//    @Bean
-//    public Job featureNotificationJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-//        return new JobBuilder(JOB_NAME, jobRepository)
-//                .start(this.sendFeatureNotificationStep(jobRepository, transactionManager))
-//                .build();
-//    }
-//
-//    @Bean(JOB_NAME + "_notificationStep")
-//    public Step sendFeatureNotificationStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-//        return new StepBuilder("notificationStep", jobRepository)
-//                .chunk(CHUNK_SIZE, platformTransactionManager)
-////                .reader(loadMemberData())
-////                .writer()
-//                .build();
-//    }
-//
-//    private ItemReader<? extends Member> loadMemberData() throws Exception {
-//        JpaPagingItemReader<Member> jpaPagingItemReader = new JpaPagingItemReaderBuilder<Member>()
-//                .name(JOB_NAME + "_loadMemberData")
-//                .entityManagerFactory(entityManagerFactory)
-//                .pageSize(CHUNK_SIZE)
-//                .queryString("select m from Member m")
-//                .build();
-//        jpaPagingItemReader.afterPropertiesSet();
-//        return jpaPagingItemReader;
-//    }
-//
-//    private ItemProcessor<? super Member, ? extends Member> checkMemberAgreement() {
-//        return members -> {
-//            if (Objects.isNull(members.getNotificationToken())) {
-//                return null;
-//            }
-//            return members;
-//        };
-//    }
-//
-//    private ItemWriter<? super Member> sendNotification() throws FirebaseMessagingException {
-//        // 가족 특징들 랜덤으로
-//        // 나랑 같은 가족
-//        return members -> members.forEach(member -> {
-//            Optional<MemberFeature> randomMemberFeature = memberFeatureRepository.findRandomMyFamilyFeature(member.getId());
-//            if (randomMemberFeature.isPresent()) {
-//                FeatureNotificationDTO.builder()
-//                        .question(randomMemberFeature.get().getFeature().getQuestion())
-//                        .answer(randomMemberFeature.get().getAnswer());
-//                try {
-//                    pushNotificationService.sendPushOneToOne(PushNotificationFCMReq.builder()
-//                            .title(Type.FEATURE.korean)
-//                            .receiverId(List.of(member.getId()))
-//                            .content(randomMemberFeature.get().getMember().getNickname()
-//                                    +randomMemberFeature.get().getFeature().getQuestion()
-//                                    +"\n"
-//                                    + randomMemberFeature.get().getAnswer())
-//                            .dataFCMReq(DataFCMReq.builder().build())
-//                            .build());
-//                } catch (FirebaseMessagingException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        });
-//    }
+    private final int CHUNK_SIZE = 1000;
+    private final String JOB_NAME = "featureNotificationJob";
+    private final EntityManagerFactory entityManagerFactory;
+    private final MemberNotificationRepository memberNotificationRepository;
+    private final PushNotificationService pushNotificationService;
+    private final MemberFeatureRepository memberFeatureRepository;
+    private final MemberFamilyRepository memberFamilyRepository;
+
+    @Bean
+    public Job featureNotificationJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
+        return new JobBuilder(JOB_NAME, jobRepository)
+                .start(this.sendFeatureNotificationStep(jobRepository, transactionManager))
+                .build();
+    }
+
+    @Bean(JOB_NAME + "_notificationStep")
+    public Step sendFeatureNotificationStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) throws Exception {
+        return new StepBuilder("notificationStep", jobRepository)
+                .<Member, Member>chunk(CHUNK_SIZE, platformTransactionManager)
+                .reader(loadMemberData())
+                .processor(checkMemberAgreement())
+                .writer(sendNotification())
+                .build();
+    }
+
+    private ItemReader<? extends Member> loadMemberData() throws Exception {
+        JpaPagingItemReader<Member> jpaPagingItemReader = new JpaPagingItemReaderBuilder<Member>()
+                .name(JOB_NAME + "_loadMemberData")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(CHUNK_SIZE)
+                .queryString("select m from Member m")
+                .build();
+        jpaPagingItemReader.afterPropertiesSet();
+        return jpaPagingItemReader;
+    }
+
+    private ItemProcessor<? super Member, ? extends Member> checkMemberAgreement() {
+        return members -> {
+            if (Objects.isNull(members.getNotificationToken())) {
+                return null;
+            }
+            return members;
+        };
+    }
+
+    private ItemWriter<? super Member> sendNotification() throws FirebaseMessagingException {
+        // 가족 특징들 랜덤으로
+        // 나랑 같은 가족
+        return members -> members.forEach(member -> {
+            Optional<MemberFeature> randomMemberFeature = memberFeatureRepository.findRandomMyFamilyFeature(member.getId());
+            if (randomMemberFeature.isPresent()) {
+                FeatureNotificationDTO.builder()
+                        .question(randomMemberFeature.get().getFeature().getQuestion())
+                        .answer(randomMemberFeature.get().getAnswer());
+                try {
+                    pushNotificationService.sendPushOneToOne(PushNotificationFCMReq.builder()
+                            .title(Type.FEATURE.korean)
+                            .receiverId(List.of(member.getId()))
+                            .content(randomMemberFeature.get().getMember().getNickname()
+                                    +randomMemberFeature.get().getFeature().getQuestion()
+                                    +"\n"
+                                    + randomMemberFeature.get().getAnswer())
+                            .dataFCMReq(DataFCMReq.builder().build())
+                            .build());
+                } catch (FirebaseMessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
 }
