2ae38d666c75cd22446ee5774a523470a691440f
yebin <yebin4684@gmail.com>
2024-05-16 09:43:41 +0900

    :bug:[FE] OnBoarding Img Modify & Submit Exam Change & Text preload

--- front/gatee/src/utils/getContentFromMission.tsx
+++ front/gatee/src/utils/getContentFromMission.tsx
@@ -2,11 +2,11 @@ const getContentFromMission = (type: string, completedLevel: number) => {
     if (type === "ALBUM")
       switch (completedLevel) {
         case 0:
-          return "어린 시절 사진 10개 채우기"
+          return "어린 시절 10개 채우기"
         case 1:
           return "가족 사진 10개 채우기"
         case 2:
-          return "내 앨범에 사진 10개 채우기"
+          return "내 앨범 사진 10개 채우기"
         default:
           return `사진 ${(completedLevel - 2) * 10}개 채우기`
       }
